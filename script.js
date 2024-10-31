f6ae3b7f83691ea7016c85750cb46bc005b025c8
accolade99 <131158528+accolade99@users.noreply.github.com>
2024-10-31 19:56:02 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,88 +1,115 @@
-function Quiz(qustions) {
+function Quiz(questions) {
     this.score = 0;
-    this.qustions = qustions;
+    this.questions = questions;
     this.currentQuestionIndex = 0;
-}
-
-Quiz.prototype.guess = function(answer) {
-    if(this.getCurrentQuestion().isCorrectAnswer(answer)) {
-        this.score++;
+  }
+  
+  Quiz.prototype.guess = function (answer) {
+    if (this.getCurrentQuestion().isCorrectAnswer(answer)) {
+      this.score++;
     }
     this.currentQuestionIndex++;
-};
-
-Quiz.prototype.getCurrentQuestion = function() {
+  };
+  
+  Quiz.prototype.getCurrentQuestion = function () {
     return this.questions[this.currentQuestionIndex];
-};
-
-Quiz.prototype.hasEnded = function() {
+  };
+  
+  Quiz.prototype.hasEnded = function () {
     return this.currentQuestionIndex >= this.questions.length;
-};
-function Question(text, choices, answer) {
+  };
+  
+  function Question(text, choices, answer) {
     this.text = text;
-    this.choices = coices;
+    this.choices = choices;
     this.answer = answer;
-}
-
-Quiz.prototype.isCorrectAnswer = function (choice) {
+  }
+  
+  Question.prototype.isCorrectAnswer = function (choice) {
     return this.answer === choice;
-};
-var QuizUI = {
+  };
+  
+  var QuizUI = {
     displayNext: function () {
-        if (quiz.hasEnded()) {
-            this.displayScore();
-        } else {
-            this.displayQuestion();
-            this.displayChoices();
-            this.displayProgress();
-        }
+      if (quiz.hasEnded()) {
+        this.displayScore();
+      } else {
+        this.displayQuestion();
+        this.displayChoices();
+        this.displayProgress();
+      }
     },
-    displayQuestion: function() {
-        this.populateIdWithHTML("question", quiz.getCurrentQuestion().text);
+    displayQuestion: function () {
+      this.populateIdWithHTML("question", quiz.getCurrentQuestion().text);
     },
-    displayChoices: function() {
-        var choices = quiz.getCurrentQuestion().choices;
-
-        for(var i = 0; i < choices.length; i++) {
-            this.populateIdWithHTML("choice" + i, choices[i]);
-            this.guessHandler("guess" + i, choices[i]);
-        }
+    displayChoices: function () {
+      var choices = quiz.getCurrentQuestion().choices;
+  
+      for (var i = 0; i < choices.length; i++) {
+        this.populateIdWithHTML("choice" + i, choices[i]);
+        this.guessHandler("guess" + i, choices[i]);
+      }
     },
-    displayScore: function() {
-        var gameOverHTML = "<h1>Game Over</h1>";
-        gameOverHTML += "<h2> Your score is: " + quiz.score + "/ 5 </h2>";
-        this.populateIdWithHTML("quiz", gameOverHTML);
+    displayScore: function () {
+      var gameOverHTML = "<h1>Game Over</h1>";
+      gameOverHTML += "<h2>Your score is: " + quiz.score + "/5</h2>";
+      this.populateIdWithHTML("quiz", gameOverHTML);
     },
-
-    populateIdWithHTML: function(id, text) {
-        var element = document.getElementById(id);
-        element.innerHTML = text;
+  
+    populateIdWithHTML: function (id, text) {
+      var element = document.getElementById(id);
+  
+      element.innerHTML = text;
     },
-    guessHandler: function(id, guess) {
-        var button = document.getElementById(id);
-        button.onclick = function() {
-            quiz.guess(guess);
-            QuizUI.displayNext();
-        }
+    guessHandler: function (id, guess) {
+      var button = document.getElementById(id);
+      button.onclick = function () {
+        quiz.guess(guess);
+        QuizUI.displayNext();
+      };
     },
-
-    displayProgress: function() {
-        var currentQuestionNumber = quiz.currentQuestionIndex + i;
-        this.populateIdWithHTML("progree", "Question" + currentQuestionNumber + "of" + quiz.question.length);
-    }
-};
-
-//create questions
-var questions = [
-    new Question("which planet has the most moons?", ["Jupitar", "Uranus", "Saturn", "Mars"], "Saturn"),
-    new Question("What country has won the most world cups?", ["Brazil", "Argentina", "England", "France"], "Brazil"),
-    new Question("How many bones are in the human ear?", ["8", "14", "5", "3"], "3"),
-    new Question("Which Netflix show had the most streaming views in 2021?", ["The witcher", "Arcane", "League of legends", "Squid Game"], "Squid Game"),
-    new Question("What is the fourth letter in th Greek alphabet?", ["Zeta", "Delta", "Alpha", "Epsilon"], "Delta")
-];
-
-//create quiz
-var quiz = new Quiz(questions);
-
-//Display Quiz
-QuizUI.displayNext();
+  
+    displayProgress: function () {
+      var currentQuestionNumber = quiz.currentQuestionIndex + 1;
+      this.populateIdWithHTML(
+        "progress",
+        "Question " + currentQuestionNumber + " of " + quiz.questions.length
+      );
+    },
+  };
+  
+  // Create questions
+  var questions = [
+    new Question(
+      "Which planet has the most moons?",
+      ["Jupiter", "Uranus", "Saturn", "Mars"],
+      "Saturn"
+    ),
+    new Question(
+      "What country has won the most World Cups?",
+      ["Brazil", "Argentina", "England", "France"],
+      "Brazil"
+    ),
+    new Question(
+      "How many bones are in the human ear?",
+      ["8", "14", "5", "3"],
+      "3"
+    ),
+    new Question(
+      "Which Netflix show had the most streaming views in 2021?",
+      ["The Witcher", "Arcane", "League of Legends", "Squid Game"],
+      "Squid Game"
+    ),
+    new Question(
+      "What is the fourth letter in the Greek alphabet?",
+      ["Zeta", "Delta", "Alpha", "Epsilon"],
+      "Delta"
+    ),
+  ];
+  
+  // Create quiz
+  var quiz = new Quiz(questions);
+  
+  // Display Quiz
+  QuizUI.displayNext();
+  
