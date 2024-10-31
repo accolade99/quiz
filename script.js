9f1fdafa0549e60ce94f3acc9ffdddad34015f9f
KINGSLEY ESISI <138278444+kingsleyesisi@users.noreply.github.com>
2024-10-31 19:34:04 +0100

    Fixed. Typo

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ Quiz.prototype.hasEnded = function() {
 };
 function Question(text, choices, answer) {
     this.text = text;
-    this.choices = coices;
+    this.choices = choices;
     this.answer = answer;
 }
 
